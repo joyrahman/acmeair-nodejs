ea8644f43b588a9291830a1004aa4c4fcc2369db
Moss Uchida <super@192.168.0.13>
2016-08-13 14:45:40 -0400

    Adding Cloudant support

--- Bluemix_Monolithic.sh
+++ Bluemix_Monolithic.sh
@@ -3,8 +3,18 @@ REGISTRY=registry.ng.bluemix.net
 NAME_SPACE=
 MONGO_BRIDGE=
 
+if ! [[ $1 == cloudant ]]
+then
+echo 'To use Cloudant DB, run the command with argument "cloudant". e.g. "./Bluemix_Monolithic.sh cloudant"'
+fi
+
 docker build -f ./acmeair-nodejs/Dockerfile_BlueMix_monolithic -t ${REGISTRY}/${NAME_SPACE}/acmeair_node_monolithic .
 
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_node_monolithic
 
+if [[ $1 == cloudant ]]
+then
+cf ic run -p 80 -e dbtype=cloudant -e CCS_BIND_APP=${MONGO_BRIDGE} --name monolithic_1 ${REGISTRY}/${NAME_SPACE}/acmeair_node_monolithic
+else
 cf ic run -p 80 -e CCS_BIND_APP=${MONGO_BRIDGE} --name monolithic_1 ${REGISTRY}/${NAME_SPACE}/acmeair_node_monolithic
+fi
