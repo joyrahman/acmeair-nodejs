3aba38ffe1dcd05c89a750606e0f9627a1fe4fe8
Moss Uchida <super@192.168.0.13>
2017-03-21 13:04:42 -0400

    Fixed code for IBM containers

--- Bluemix_Container.sh
+++ Bluemix_Container.sh
@@ -1,11 +1,20 @@
 cd ..
 REGISTRY=registry.ng.bluemix.net
-NAME_SPACE=
-MONGO_BRIDGE=
+NAME_SPACE=strong
+MONGO_BRIDGE=MongoBridgeMoss
 
+if ! [[ $1 == group ]]
+then
+echo 'To use Container Group, run the command with argument "group". e.g. "./Bluemix_Container.sh group"'
+fi
 
 docker build -f ./acmeair-nodejs/Dockerfile -t ${REGISTRY}/${NAME_SPACE}/acmeair_node .
 
 docker push ${REGISTRY}/${NAME_SPACE}/acmeair_node
 
-cf ic run -p 80 -e CCS_BIND_APP=${MONGO_BRIDGE} --name acmeair_node ${REGISTRY}/${NAME_SPACE}/acmeair_node
+if ! [[ $1 == group ]]
+then
+cf ic run -p 80 -m 64 -e CCS_BIND_APP=${MONGO_BRIDGE} --name acmeair_node ${REGISTRY}/${NAME_SPACE}/acmeair_node
+else
+cf ic group create -p 80 -m 64 --min 1 --desired 1 --auto -e CCS_BIND_APP=${MONGO_BRIDGE} --name acmeair_node_group -n acmeairnode -d mybluemix.net ${REGISTRY}/${NAME_SPACE}/acmeair_node
+fi
